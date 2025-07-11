b5eedea9ef0248c85b0159524e96c72e71a43f8a
Rocael14 <jc.vmayen@udeo.edu.gt>
2025-07-11 10:21:20 -0600

    Se realizo el case uno para registrar estudiantes en la lista

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ class Estudiante:
 
     def mostrar_informacion(self):
         print(f"Nombre: {self.nombre}, Carnet: {self.carne}, Carrera: {self.carrera}")
-
+    def registro_estudiante(self):
+        print(f"Se registro correctamente al estudiante {self.nombre}")
 
 def Menu():
     print("---MENU---")
@@ -18,10 +19,31 @@ def Menu():
     print("5. Salir")
     print("------------")
 
-
+estudiantes = []
 while True:
     Menu()
     try:
         opcion = int(input("Ingrese una opcion:"))
+        match opcion:
+            case 1:
+                print("--- Registrar Estudiante ---")
+                nombre_estudiante = input("Ingrese nombre: ")
+                carne_estudiante = input("Ingrese carne: ")
+                carrera_estudiante = input("Ingrese carrera: ")
+                nota_final = input("Ingrese nota final: ")
+                estudiante = Estudiante(nombre_estudiante, carne_estudiante, carrera_estudiante, nota_final)
+                estudiantes.append(estudiante)
+                estudiante.registro_estudiante()
+            case 2:
+                print("Estudiante registrado correctamente")
+            case 3:
+                print("Buscar Estudiante")
+            case 4:
+                print("Calcular Promedio")
+            case 5:
+                print("Gracias por utilizar el programa")
+                break
+            case _:
+                print("Opcion no valida")
     except Exception:
         print("La opcion debe ser numerica")
