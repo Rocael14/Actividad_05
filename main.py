4f99783108aca1520e823434ddc38c237a677e2a
Rocael14 <jc.vmayen@udeo.edu.gt>
2025-07-11 10:15:22 -0600

    Se realizo el menu con sus opciones

--- main.py
+++ main.py
@@ -8,3 +8,20 @@ class Estudiante:
     def mostrar_informacion(self):
         print(f"Nombre: {self.nombre}, Carnet: {self.carne}, Carrera: {self.carrera}")
 
+
+def Menu():
+    print("---MENU---")
+    print("1. Registrar Estudiante")
+    print("2. Lista de Estudiantes")
+    print("3. Buscar Estudiante")
+    print("4. Calcular Promedio")
+    print("5. Salir")
+    print("------------")
+
+
+while True:
+    Menu()
+    try:
+        opcion = int(input("Ingrese una opcion:"))
+    except Exception:
+        print("La opcion debe ser numerica")
